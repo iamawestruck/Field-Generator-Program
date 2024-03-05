f45f588a72507406c06b03dc00469f2475ed8725
Quinn Morrissey <q.t.morrissey@gmail.com>
2024-03-05 15:23:06 -0800

    Added beginning of EquationListWidget with add and remove functionality built out, along with x and y buttons.

--- GUI.py
+++ GUI.py
@@ -13,14 +13,90 @@ class MplCanvas(FigureCanvasQTAgg):
         self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
 
+class EquationWidget(QtWidgets.QWidget):
+    def __init__(self, equationString, equationLambda):
+        super().__init__()
+        self.equationString = equationString
+        self.equationLambda = equationLambda
+        self.xButton = QtWidgets.QRadioButton()
+        self.yButton = QtWidgets.QRadioButton()
+        self.buttons = QtWidgets.QWidget()
+        self.buttons.layout = QtWidgets.QHBoxLayout()
+        self.buttons.layout.addWidget(self.xButton)
+        self.buttons.layout.addWidget(self.yButton)
+        self.buttons.setLayout(self.buttons.layout)
+        self.equationLabel = QtWidgets.QLabel()
+        self.equationLabel.setText(equationString)
+        self.deleteButton = QtWidgets.QPushButton("-")
+        widgetLayout = QtWidgets.QHBoxLayout()
+        widgetLayout.addWidget(self.buttons, alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
+        widgetLayout.addWidget(self.equationLabel)
+        widgetLayout.addWidget(self.deleteButton, alignment=QtCore.Qt.AlignmentFlag.AlignRight)
+        # self.xButton.hide()
+        self.setLayout(widgetLayout)
+
+
+class EquationListWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.equationWidgets = {}
+        self.xButtonGroup = QtWidgets.QButtonGroup()
+        self.yButtonGroup = QtWidgets.QButtonGroup()
+        layout = QtWidgets.QVBoxLayout()
+        self.standardLabelWidget = QtWidgets.QLabel(" dy/dx ")
+        self.parametricLabelWidget = QtWidgets.QLabel(" dx/dt  dy/dt ")
+        layout.addWidget(self.parametricLabelWidget, alignment=QtCore.Qt.AlignmentFlag.AlignTop | QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.setLayout(layout)
+
+    def addEquation(self, equationString, equationLambda):
+        currentEquation = EquationWidget(equationString, equationLambda)
+        currentEquation.deleteButton.clicked.connect(lambda: self.removeEquation(equationString))
+        self.equationWidgets[equationString] = currentEquation
+        self.xButtonGroup.addButton(currentEquation.xButton)
+        self.yButtonGroup.addButton(currentEquation.yButton)
+        layout = self.layout()
+        layout.addWidget(currentEquation, alignment=QtCore.Qt.AlignmentFlag.AlignTop)
+        self.setLayout(layout)
+
+    def removeEquation(self, equationString):
+        if equationString not in self.equationWidgets.keys():
+            return
+        currentEquation = self.equationWidgets[equationString]
+        currentEquation.hide()
+        layout = self.layout()
+        layout.removeWidget(currentEquation)
+        self.setLayout(layout)
+
+
+class EquationListGroupBox(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.equationListWidget = EquationListWidget()
+        self.standardParametricWidget = None
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.equationListWidget)
+        # layout.addWidget(self.standardParametricWidget)
+        self.setLayout(layout)
+        self.show()
+
+    def addEquation(self, equationString, equationLambda):
+        self.equationListWidget.addEquation(equationString, equationLambda)
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
-        self.setCentralWidget(sc)
+        # sc = MplCanvas(self, width=5, height=4, dpi=100)
+        # sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
+        # self.setCentralWidget(sc)
 
+        self.equationListGroupBox = EquationListGroupBox()
+        self.equationListGroupBox.addEquation("x+x", lambda x,y: x+x)
+        self.equationListGroupBox.addEquation("x+y", lambda x,y: x+y)
+        self.equationListGroupBox.addEquation("x+y^2", lambda x, y: x + y*y)
+        self.equationListGroupBox.addEquation("x+y*2", lambda x, y: x + y*2)
+        self.setCentralWidget(self.equationListGroupBox)
         self.show()
 
 app = QtWidgets.QApplication(sys.argv)
